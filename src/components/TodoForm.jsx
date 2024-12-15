d9a78a1dd2c8eb96d1a6b611daff859b6ae1bfd6
Jecheon Ryu <rjc1704@gmail.com>
2024-12-15 22:45:30 +0900

    일부 수정

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -40,25 +40,33 @@ export default function TodoForm() {
   };
 
   return (
-    <form onSubmit={handleAddTodo}>
-      <label htmlFor="title">제목:</label>
-      <input
-        type="text"
-        id="title"
-        name="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        required
-      />
-      <label htmlFor="contents">내용:</label>
-      <input
-        id="contents"
-        name="contents"
-        value={contents}
-        onChange={(e) => setContents(e.target.value)}
-        required
-      />
-      <button type="submit">추가하기</button>
+    <form className="todo-form" onSubmit={handleAddTodo}>
+      <div className="form-group">
+        <label htmlFor="title">제목:</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          className="form-input"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="contents">내용:</label>
+        <input
+          id="contents"
+          name="contents"
+          value={contents}
+          onChange={(e) => setContents(e.target.value)}
+          required
+          className="form-input"
+        />
+      </div>
+      <button type="submit" className="submit-button">
+        추가하기
+      </button>
     </form>
   );
 }
